8864b665434ecfd3942fd1e9b1c2c4b797de003b
Zheng Huang <zheng.huang@ndsu.edu>
2016-09-15 12:00:16 -0500

    second version of hello world, add app.get add.post

--- web server/Example1/server.js
+++ web server/Example1/server.js
@@ -3,14 +3,20 @@
  */
 // import the http module(api)
 var http = require('http');
-
+var dispatcher = require('httpdispatcher');
 
 // define the constant port number
 const PORT= 8090;
 
-function handlerRequest(request, response)
-{
-    response.end('It works!! path hit:'+ request.url);
+function handleRequest(request, response){
+    try {
+        //log the request on console
+        console.log(request.url);
+        //Disptach
+        dispatcher.dispatch(request, response);
+    } catch(err) {
+        console.log(err);
+    }
 }
 //create a server
 var server = http.createServer(handlerRequest);
